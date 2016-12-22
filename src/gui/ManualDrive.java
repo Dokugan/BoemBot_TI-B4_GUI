a19c73f7a76188dcd4d68072ea01bffdd133dcc2
Rick-Farstreet <ricky-1999@live.nl>
2016-12-22 14:53:24 +0100

    added Manual drive form

---
+++ src/gui/ManualDrive.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Rick Verstraten on 22-12-2016.
+ */
+public class ManualDrive {
+    private JPanel mainPanel;
+    private JButton btnRight;
+    private JButton btnForward;
+    private JButton btnLeft;
+    private JButton btnBack;
+}
