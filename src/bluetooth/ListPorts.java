80bc6cf828217e27365fc0104b4f7cafc7c40c5a
dokugan <stijn-28@hotmail.com>
2017-01-17 10:31:27 +0100

    + undone prev change

--- src/bluetooth/ListPorts.java
+++ src/bluetooth/ListPorts.java
@@ -1,5 +1,6 @@
 package bluetooth;
 
+import gnu.io.*;
 import jssc.SerialPortList;
 import gui.Main;
 import gui.NewBoebot;
@@ -11,21 +12,21 @@ import java.util.Objects;
 public class ListPorts {
     public static void getPorts()
     {
-//        java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
-//        while ( portEnum.hasMoreElements() )
-//        {
-//            if(NewBoebot.ports.contains("Poorten laden...")) {
-//                NewBoebot.ports.clear();
-//            }
-//
-//            CommPortIdentifier portIdentifier = portEnum.nextElement();
-//                if (Objects.equals(getPortTypeName(portIdentifier.getPortType()), "Serial")) {
-//                    Main.mp.newBoebotPanel.ports.add(portIdentifier.getName());
-//                }
-//            System.out.println(portIdentifier.getName()  +  " - " +  getPortTypeName(portIdentifier.getPortType()));
-//
-//        }
-        NewBoebot.ports = new ArrayList(Arrays.asList(SerialPortList.getPortNames()));
+        java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
+        while ( portEnum.hasMoreElements() )
+        {
+            if(NewBoebot.ports.contains("Poorten laden...")) {
+                NewBoebot.ports.clear();
+            }
+
+            CommPortIdentifier portIdentifier = portEnum.nextElement();
+                if (Objects.equals(getPortTypeName(portIdentifier.getPortType()), "Serial")) {
+                    Main.mp.newBoebotPanel.ports.add(portIdentifier.getName());
+                }
+            System.out.println(portIdentifier.getName()  +  " - " +  getPortTypeName(portIdentifier.getPortType()));
+
+        }
+//        NewBoebot.ports = new ArrayList(Arrays.asList(SerialPortList.getPortNames()));
 
         if(Main.mp.newBoebotPanel.ports.size() == 0){
             Main.mp.newBoebotPanel.ports.add("Geen poorten beschikbaar");
@@ -34,22 +35,22 @@ public class ListPorts {
         System.out.println("done");
     }
 
-//    private static String getPortTypeName ( int portType )
-//    {
-//        switch ( portType )
-//        {
-//            case CommPortIdentifier.PORT_I2C:
-//                return "I2C";
-//            case CommPortIdentifier.PORT_PARALLEL:
-//                return "Parallel";
-//            case CommPortIdentifier.PORT_RAW:
-//                return "Raw";
-//            case CommPortIdentifier.PORT_RS485:
-//                return "RS485";
-//            case CommPortIdentifier.PORT_SERIAL:
-//                return "Serial";
-//            default:
-//                return "unknown type";
-//        }
-//    }
+    private static String getPortTypeName ( int portType )
+    {
+        switch ( portType )
+        {
+            case CommPortIdentifier.PORT_I2C:
+                return "I2C";
+            case CommPortIdentifier.PORT_PARALLEL:
+                return "Parallel";
+            case CommPortIdentifier.PORT_RAW:
+                return "Raw";
+            case CommPortIdentifier.PORT_RS485:
+                return "RS485";
+            case CommPortIdentifier.PORT_SERIAL:
+                return "Serial";
+            default:
+                return "unknown type";
+        }
+    }
 }
